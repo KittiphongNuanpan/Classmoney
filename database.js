c7ff44c470acc14888a9c8b15a34a751b845f03f
KittiphongNuanpan <142085595+KittiphongNuanpan@users.noreply.github.com>
2026-06-09 10:50:25 +0700

    Fix connection string format in database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 // วางลิงก์ที่ได้จาก Transaction pooler ตรงนี้ครับ (เผื่อเวลารันเทสต์ในคอมตัวเอง)
-const supabaseUrl = process.env.DATABASE_URL || "postgresql://postgres.gbncpprvrytzifyzbtnr:[123789147369159#Ss]@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres";
+const supabaseUrl = process.env.DATABASE_URL || "postgresql://postgres.gbncpprvrytzifyzbtnr:123789147369159@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres";
 
 const pool = new Pool({
     connectionString: supabaseUrl, 

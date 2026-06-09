d839e77a9e357f1fd22bcb4dc469012ec57d668a
KittiphongNuanpan <142085595+KittiphongNuanpan@users.noreply.github.com>
2026-06-09 10:25:06 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg');
 
+// วางลิงก์ที่ได้จาก Transaction pooler ตรงนี้ครับ (เผื่อเวลารันเทสต์ในคอมตัวเอง)
+const supabaseUrl = process.env.DATABASE_URL || "postgresql://postgres.gbncpprvrytzifyzbtnr:[123789147369159#Ss]@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres";
+
 const pool = new Pool({
-    // ตรงบรรทัดล่างนี้ เวลาเทสต์ในคอม ให้เอาลิงก์ Supabase ของคุณมาใส่ในเครื่องหมายคำพูดแทน process.env.DATABASE_URL ได้เลยครับ
-    connectionString: process.env.DATABASE_URL, 
+    connectionString: supabaseUrl, 
     ssl: { rejectUnauthorized: false }
 });
 
